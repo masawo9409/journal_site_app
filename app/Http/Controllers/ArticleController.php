f4a5d7d5dba1535a075892f3d07a10b53c97ff76
masawo9409 <masawo9409@gmail.com>
2022-10-17 23:20:01 +0900

    課題2完了

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -6,12 +6,12 @@ use Illuminate\Http\Request;
 use App\Models\Article;
 
 class ArticleController extends Controller
-{   
+{
     // showページへ移動
     public function show($id)
     {
         $article = Article::find($id);
-        return view('articles.show',['article' => $article]);
+        return view('articles.show', ['article' => $article]);
     }
 
     public function index()
@@ -19,6 +19,27 @@ class ArticleController extends Controller
         //モデル名::テーブル全件取得
         $article = Article::all();
         //
-        return view('articles.index',['article'=> $article]);
+        return view('articles.index', ['article' => $article]);
+    }
+
+    public function create()
+    {
+        return view('articles.create');
+    }
+
+
+
+    public function store(Request $request)
+    {
+        //Modelsクラスの中でインスタンスの作成
+        $article = new Article;
+
+        //値の用意
+        $article->title = $request->title;
+        $article->body  = $request->body;
+
+        $article->save();
+        
+        return redirect('/articles');
     }
 }
